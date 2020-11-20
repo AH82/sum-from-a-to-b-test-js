371c766aafc90ac0c003cc45e33d8fc4bc672710
Hatem <hatem.deux@gmail.com>
2020-11-20 15:17:27 -0500

    adds header comment

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+/* AH82:
+  * Originally implemented during bootcamp on 2020-02-07
+  * Remote repo created post-bootcamp on 2020-11-20 (due forgotton forking error and time constrains)
+  * linted post-bootcamp on 2020-11-20
+*/
 
 function sum(fromN, toN) {
   // Sum all the values from fromN up to toN
